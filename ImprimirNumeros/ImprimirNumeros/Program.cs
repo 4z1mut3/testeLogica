9507573093f41f725e03c3ec69d235fa9d7a9872
Paulo André Oliveira <andre.oliveira.belo@gmail.com>
2021-11-11 13:37:53 -0300

    Respostas do teste

---
+++ ImprimirNumeros/ImprimirNumeros/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ImprimirNumeros
+{
+
+
+    //    TESTE 02
+    //Escrever um programa que escreva na tela números de 1 a 100.
+
+    //ESCREVER O TESTE EM C#
+    //NÃO pode: utilizar laços de repetição, por exemplo: do-while, for, foreach, linq, lambda e etc;
+    //Será avaliado: Lógica de programação;
+
+    //Classe criada para montar o objeto do que será impresso na tela 
+    class Imprime {
+        
+        public int valorInicial { get; set; }
+        public int valorFinal { get; set; }
+
+        public Imprime() { }
+        public Imprime(int valorInicial, int valorFinal) {
+            this.valorInicial = valorInicial;
+            this.valorFinal = valorFinal;
+        }
+
+        //imprime na tela a sequencia de numeros desejada, incrementando de 1 em 1. Este metodo faz uso de recursividade. 
+        public int imprimeNumeros(int inicial,int final)
+        {
+            if (inicial <= final)
+            {
+                inicial++;
+                Console.WriteLine(inicial);
+                imprimeNumeros(inicial, final);
+                return inicial;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+
+
+    class Program
+    {        
+        static void Main(string[] args)
+        {   
+            //Criando objeto e chamento método de impressão
+            Imprime im = new Imprime(0,100);
+            im.imprimeNumeros(im.valorInicial,im.valorFinal);
+        }
+    }
+}
---
+++ problemaArray_01/problemaArray_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace problemaArray_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* TESTE_01
+             * Dado o a variavel arrInicial que é um array com os valores[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].
+             * Gravar na variavel arrFinal os mesmos valores, porem em ordem invertida.
+             */
+            
+            int[] arrayInicial = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int[] arrayFinal = new int[10];
+            int auxCounter = 0;
+            
+            //Faz a inversão dos arrays
+            for (int i = arrayInicial.Length -1; i >= 0;i--) {
+                arrayFinal[auxCounter] = arrayInicial[i];
+                auxCounter++;
+            }
+
+            //Imprime o array invertido na tela
+            for (int k = 0;k < arrayFinal.Length;k++) {
+                Console.WriteLine(arrayFinal[k]);    
+            }
+
+        }
+    }
+}
